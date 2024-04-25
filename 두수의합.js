5239fbf44f376506c34e7f05bce459852b6dfee4
Kimxogus0108 <80230538+Kimxogus0108@users.noreply.github.com>
2024-04-25 14:07:02 +0900

    두수의합

---
+++ 두수의합.js
@@ -0,0 +1,10 @@
+function solution(num1, num2) {
+    var answer;
+    if((-50000<= num1 && num1 <=50000)&&(-50000<= num2 && num2 <=50000)){
+        answer = num1 + num2;
+        return answer;
+
+    }
+}
+
+console.log(solution(2,3))
